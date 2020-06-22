b67119fa3c3f8652454ded59b88d30fd6e41413a
Areeq Hasan <ahasan20@lawrenceville.org>
2020-06-22 14:12:15 -0400

    Update segment.py

---
+++ Algorithms/Single/segment.py
@@ -0,0 +1,34 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #                                                                                                  #
+#                                                   _                           #
+#    ___    ___    __ _   _ __ ___     ___   _ __   | |_       _ __    _   _    #
+#   / __|  / _ \  / _` | | '_ ` _ \   / _ \ | '_ \  | __|     | '_ \  | | | |   #
+#   \__ \ |  __/ | (_| | | | | | | | |  __/ | | | | | |_   _  | |_) | | |_| |   #
+#   |___/  \___|  \__, | |_| |_| |_|  \___| |_| |_|  \__| (_) | .__/   \__, |   #
+#                 |___/                                       |_|      |___/    #
+#                                                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Est. June 20, 2020 (6/20/2020)  #
+# # # # # # # # # # # # # # # # # #
+# SINA BIOMEDICAL RESEARCH GROUP  #
+#   +-- S. Kaisar Alam (PI)       #
+#   +-- Areeq I. Hasan (Lead)     #
+#   +-- Sarina M. Hasan           #
+#   +-- Raiyah Z. Ahmed           #
+#   +-- Wasi S. Ahmed             #
+# # # # # # # # # # # # # # # # # #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# compare_area()                                                                              #
+# Computes the ratio between the area of a lesion in a given b-mode and strain image pair.    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# @ params                                                                                    #
+#   +-- image_directory: (string) | Path to the directory containing strain.png & b-mode.png  #
+# @ output                                                                                    #
+#   +-- area_ratio:      (double) | the area ratio between strain and b-mode masks.           #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+def segment(image, type):
+
+    segmented_image = ""
+
+    return segmented_image
