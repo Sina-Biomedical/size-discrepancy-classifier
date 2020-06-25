84b3628f04009b0fe95f8258ce2e2c5472e69dea
Areeq Hasan <ahasan20@lawrenceville.org>
2020-06-25 16:06:06 -0400

    working!

--- Algorithms/Single/segmentation_methods.py
+++ Algorithms/Single/segmentation_methods.py
@@ -39,16 +39,25 @@ def RegionGrowing(image, portion, epsilon):
     output = np.zeros(np.shape(image), dtype=np.uint8)
     output[x][y] = 255
     while len(portion) > 0:
-         xcd = portion[0][0]
-         ycd = portion[0][1]
-         for i in range(-1, 2):
-              for j in range(-1, 2):
+        xcd = portion[0][0]
+        ycd = portion[0][1]
+        for i in range(-1, 2):
+            for j in range(-1, 2):
                 absDiff = abs(int(cen_pix) - int(image[xcd + i][ycd + j]))
                 if (i != 0 or j != 0) and output[xcd + i][ycd + j] != 255 and absDiff < epsilon:
                     output[xcd + i][ycd + j] = 255
                     portion.append([xcd + i, ycd + j])
-         portion.pop(0)
+        portion.pop(0)
     return output
+
+def RemoveUnwantedRegions(output):
+    new_img = np.zeros_like(output)
+    for val in np.unique(output)[1:]:
+        mask = np.uint8(output == val)  # step 3
+        labels, stats = cv2.connectedComponentsWithStats(mask, 5)[1:3]
+        largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+        new_img[labels == largest_label] = val
+    return new_img
 
 def segment(image, portion, type, epsilon):
     RG = RegionGrowing(image, portion, epsilon)
@@ -56,7 +65,7 @@ def segment(image, portion, type, epsilon):
     RG = cv2.morphologyEx(RG, cv2.MORPH_CLOSE, kernel, iterations=2)
     # Removing small regions that are not a part of lesion like corners
     segmented_image = RemoveUnwantedRegions(RG)
-    cv2.imshow('Output', output)
+    cv2.imshow('Output', segmented_image)
     cv2.waitKey()
 
     return segmented_image
