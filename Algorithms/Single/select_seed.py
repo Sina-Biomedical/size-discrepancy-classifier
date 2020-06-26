311e4de4f60e1557b2d57c2cdace0ebfd460c8d3
Areeq Hasan <ahasan20@lawrenceville.org>
2020-06-26 15:52:41 -0400

    Update select_seed.py

--- Algorithms/Single/select_seed.py
+++ Algorithms/Single/select_seed.py
@@ -49,11 +49,12 @@ def select_seed(image_filepath):
     processed_image, _ = preprocess(image_filepath)
 
     if image_type == "strain":
-        ret,thresh1 = cv2.threshold(processed_image,127,255,cv2.THRESH_BINARY)
-        ret,thresh2 = cv2.threshold(processed_image,127,255,cv2.THRESH_BINARY_INV)
-        ret,thresh3 = cv2.threshold(processed_image,127,255,cv2.THRESH_TRUNC)
-        ret,thresh4 = cv2.threshold(processed_image,127,255,cv2.THRESH_TOZERO)
-        ret,thresh5 = cv2.threshold(processed_image,127,255,cv2.THRESH_TOZERO_INV)
+        threshold = 70
+        ret,thresh1 = cv2.threshold(processed_image,threshold,255,cv2.THRESH_BINARY)
+        ret,thresh2 = cv2.threshold(processed_image,threshold,255,cv2.THRESH_BINARY_INV)
+        ret,thresh3 = cv2.threshold(processed_image,threshold,255,cv2.THRESH_TRUNC)
+        ret,thresh4 = cv2.threshold(processed_image,threshold,255,cv2.THRESH_TOZERO)
+        ret,thresh5 = cv2.threshold(processed_image,threshold,255,cv2.THRESH_TOZERO_INV)
 
         titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
         images = [processed_image, thresh1, thresh2, thresh3, thresh4, thresh5]
