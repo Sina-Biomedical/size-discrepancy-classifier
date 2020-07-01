00f3df4ec36559746ddb5e52f1687d3ff7693fda
Areeq Hasan <ahasan20@lawrenceville.org>
2020-07-01 16:09:19 -0400

    cleanup

--- Algorithms/Single/segmentation_methods.py
+++ Algorithms/Single/segmentation_methods.py
@@ -35,52 +35,6 @@ def fill_contours(arr):
     return np.maximum.accumulate(arr,1) & \
            np.maximum.accumulate(arr[:,::-1],1)[:,::-1]
 
-# hyperparameters = {
-#     'strain': {
-#         'preprocessing_methods': {
-#             'image_cropping': [0, 470, 100, 700],
-#             'image_resizing':  0.4,
-#             'closing_size'  :  12,
-#             'closing_iters' :  1,
-#             'med_blurring'  :  59
-#         },
-#         'lesion_locating_methods': {
-#             'threshold_1'   :  193,
-#             'threshold_2'   :  194,
-#         },
-#         'segmentation_methods': {
-#             'bin_threshold' :  84,
-#             'open/dil_size' :  3,
-#             'open_iters'    :  2,
-#             'dil_iters'     :  3,
-#             'dist_transform':  5,
-#             'fg_thresh'     :  0.7
-#         },
-#     },
-#     'bmode': {
-#         'preprocessing_methods': {
-#             'image_cropping': [0, 600, 100, 700],
-#             'image_resizing': 0.4,
-#             'closing_size'  : 13,
-#             'closing_iters' : 1,
-#             'bilat_filter'  : 13,
-#             'med_blurring'  : 25
-#         },
-#         'lesion_locating_methods': {
-#             'threshold_1'   : 193,
-#             'threshold_2'   : 194
-#         },
-#         'segmentation_methods': {
-#             'bin_threshold' : 212,
-#             'open/dil_size' : 3,
-#             'open_iters'    : 2,
-#             'dil_iters'     : 3,
-#             'dist_transform': 5,
-#             'fg_thresh'     : 0.7
-#         }
-#     }
-# }
-
 def segment(image, image_type, hyperparameters):
 
     if image_type == 'strain':
@@ -97,67 +51,79 @@ def segment(image, image_type, hyperparameters):
         # DETERMINE SURROUNDING TISSUE
         kernel = np.ones((open_dil_size, open_dil_size), np.uint8)
         opening = cv2.morphologyEx(binarized_image, cv2.MORPH_OPEN, kernel, open_iters)
-        sure_bg = cv2.dilate(opening, kernel, dil_iters)
+        background_region = cv2.dilate(opening, kernel, dil_iters)
 
-        # DISTANCE TRANSFORM
+        # DISTANCE TRANSFORMATION FOR WATERSHED
         dist_transform_param = hyperparameters['strain']['segmentation_methods']['dist_transform']
         dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, dist_transform_param)
 
+        # DETERMINE LESION FOREGROUND
         fg_threshold = hyperparameters['strain']['segmentation_methods']['fg_thresh']
-        _, sure_fg = cv2.threshold(dist_transform, fg_threshold * dist_transform.max(), 255, 0)
-        sure_fg = np.uint8(sure_fg)
+        _, foreground_region = cv2.threshold(dist_transform, fg_threshold * dist_transform.max(), 255, 0)
+        foreground_region = np.uint8(foreground_region)
 
-        unknown = cv2.subtract(sure_bg, sure_fg)
+        # SUBTRACT BACKGROUND FROM FOREGROUND
+        watershed_region = cv2.subtract(background_region, foreground_region)
 
-        _, markers = cv2.connectedComponents(sure_fg)
+        # DETERMINE WATERSHED MARKERS
+        _, markers = cv2.connectedComponents(foreground_region)
         markers = markers + 1
-        markers[unknown == 255] = 0
+        markers[watershed_region == 255] = 0
 
-        output_image = image
-
+        # SUPERIMPOSE WATERSHED MARKERS
+        segmented_image = image
         markers = cv2.watershed(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB), markers)
         binarized_image = fill_contours(markers)
-        output_image[markers == -1] = 255
+        segmented_image[markers == -1] = 255
 
+        # DETERMINE LESION AREA
         image_resizing = hyperparameters['strain']['preprocessing_methods']['image_resizing']
         strain_area = np.sum(binarized_image == 2) * ((1/image_resizing) ** 2)
 
         plt.imshow(binarized_image, cmap='gray')
         plt.show()
 
+        # RESIZE & PAD IMAGE --> ORIGINAL DIMENSIONS
         original_dims = [0, 600, 0,   800]
         image_cropping = hyperparameters['strain']['preprocessing_methods']['image_cropping']
         padding = [ (abs(original_dims[0] - image_cropping[0]), abs(original_dims[1] - image_cropping[1])), (abs(original_dims[2] - image_cropping[2]), abs(original_dims[3] - image_cropping[3]))]
 
-        return strain_area, np.pad(cv2.resize(output_image, (0,0), fx = 1/image_resizing, fy = 1/image_resizing), padding, mode='constant', constant_values=0)
+        return strain_area, np.pad(cv2.resize(segmented_image, (0,0), fx = 1/image_resizing, fy = 1/image_resizing), padding, mode='constant', constant_values=0)
     else:
 
+        # IMAGE THRESHOLDING
         bin_threshold = hyperparameters['bmode']['segmentation_methods']['bin_threshold']
         _, binarized_image = cv2.threshold(image, bin_threshold, 255, cv2.THRESH_BINARY_INV)
 
+        # MORPHOLOGICAL OPENING
         open_dil_size = hyperparameters['bmode']['segmentation_methods']['open/dil_size']
         open_iters = hyperparameters['bmode']['segmentation_methods']['open_iters']
         kernel = np.ones((open_dil_size, open_dil_size), np.uint8)
         opening = cv2.morphologyEx(binarized_image, cv2.MORPH_OPEN, kernel, iterations = open_iters)
 
+        # MORPHOLOGICAL DILATION
         dil_iters = hyperparameters['bmode']['segmentation_methods']['dil_iters']
-        sure_bg = cv2.dilate(opening, kernel, iterations = dil_iters)
+        background_region = cv2.dilate(opening, kernel, iterations = dil_iters)
 
+        # DISTANCE TRANSFORMATION FOR WATERSHED
         dist_transform = hyperparameters['bmode']['segmentation_methods']['dist_transform']
         dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, dist_transform)
 
+        # DETERMINE LESION FOREGROUND
         fg_thresh = hyperparameters['bmode']['segmentation_methods']['fg_thresh']
-        _, sure_fg = cv2.threshold(dist_transform, fg_thresh * dist_transform.max(), 255, 0)
+        _, foreground_region = cv2.threshold(dist_transform, fg_thresh * dist_transform.max(), 255, 0)
 
-        sure_fg = np.uint8(sure_fg)
-        unknown = cv2.subtract(sure_bg, sure_fg)
+        # SUBTRACT BACKGROUND FROM FOREGROUND
+        foreground_region = np.uint8(foreground_region)
+        unknown = cv2.subtract(background_region, foreground_region)
 
-        _, markers = cv2.connectedComponents(sure_fg)
+        # DETERMINE WATERSHED MARKERS
+        _, markers = cv2.connectedComponents(foreground_region)
         markers = markers + 1
         markers[unknown == 255] = 0
 
+        # SUPERIMPOSE WATERSHED MARKERS
         output_image = image
-
         markers = cv2.watershed(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB), markers)
         binarized_image = fill_contours(markers)
         output_image[markers == -1] = 255
@@ -165,6 +131,7 @@ def segment(image, image_type, hyperparameters):
         plt.imshow(binarized_image, cmap='gray')
         plt.show()
 
+        #  DETERMINE LESION AREA
         bmode_area = np.sum(binarized_image == 2)
 
         return bmode_area, output_image
