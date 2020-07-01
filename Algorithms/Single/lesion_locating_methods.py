214cba5f61347d7f7dd4b4fa1adcaaa4ef2e132d
Areeq Hasan <ahasan20@lawrenceville.org>
2020-07-01 16:13:45 -0400

    CLEANUP

--- Algorithms/Single/lesion_locating_methods.py
+++ Algorithms/Single/lesion_locating_methods.py
@@ -3,17 +3,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 import copy
 
-def find_lesion(image_src, hyperparameters):
-    gray = image_src
+def find_lesion(image, hyperparameters):
 
+    # THRESHOLD IMAGE
     bin_threshold = hyperparameters['bmode']['lesion_locating_methods']['bin_threshold']
-    ret, gray = cv2.threshold(gray, bin_threshold, 255, 0)
+    ret, binarized_image = cv2.threshold(image, bin_threshold, 255, 0)
 
-    contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    # FIND IMAGE CONTOURS
+    contours, hierarchy = cv2.findContours(binarized_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # FIND LARGEST CONTOUR
     largest_area = sorted(contours, key=cv2.contourArea)[-1]
-    mask = np.zeros(image_src.shape, np.uint8)
+    mask = np.zeros(image.shape, np.uint8)
+
+    # MASK IMAGE W/ LARGEST CONTOUR
     cv2.drawContours(mask, [largest_area], 0, (255,255,255,255), -1)
-    dst = cv2.bitwise_and(image_src, mask)
+    dst = cv2.bitwise_and(image, mask)
     mask = 255 - mask
     roi = cv2.add(dst, mask)
 
